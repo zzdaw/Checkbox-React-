158adaef611bae682e43b73bda5d055ea6541ad2
zzdaw <hohi@wp.pl>
2019-01-07 17:48:59 +0100

    added validation after click the button

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-const PositiveMessage = () => <p>You can buy ticket.</p>
-const NegativeMessage = () => <p>You can't buy ticket.</p>
+const SelectionMessages = (props) => <p>{props.txt}</p>
+
+// const PositiveMessage = () => <p>You can buy ticket.</p>
+// const NegativeMessage = () => <p>You can't buy ticket.</p>
 
 class Checkbox extends React.Component {
     state = {
-        isConfirmed: false
+        isConfirmed: false,
+        isSubmitted: false
     }
     handleChecked = () => {
         this.setState({
-            isConfirmed: !this.state.isConfirmed
+            isConfirmed: !this.state.isConfirmed,
+            isSubmitted: false
         })
     }
     displayMessage = () => {
-        if (this.state.isConfirmed) {
-            return <PositiveMessage />
-        } else {
-            return <NegativeMessage />
+        if (this.state.isSubmitted) {
+            if (this.state.isConfirmed) {
+                return <SelectionMessages txt='You can buy ticket.' />
+            } else {
+                return <SelectionMessages txt="You can't buy ticket." />
+            }
         }
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.setState({
+            isSubmitted: true
+        })
     }
     render() {
         const { isConfirmed } = this.state
         return (
             <>
                 <h1>Confirms your age.</h1>
-                <input type="checkbox" id='age' checked={isConfirmed} onChange={this.handleChecked} />
-                <label htmlFor="age">I have at least 16 years old.</label>
-                {this.displayMessage()}
+                <form action="" onSubmit={this.handleSubmit}>
+                    <input type="checkbox" id='age' checked={isConfirmed} onChange={this.handleChecked} />
+                    <label htmlFor="age">I have at least 16 years old.</label><br />
+                    <button>Click</button>
+                    {this.displayMessage()}
+                </form>
             </>
         )
     }
